c7689b8cd11c31cd23d3249a69c6bcc906d8d3a1
Usuário <kaqueivaicorinthians@gmail.com>
2023-12-07 12:18:49 -0300

    Primeiro trabalho

---
+++ codigo.js
@@ -0,0 +1,110 @@
+function Inicio()
+{
+    let inicio= document.getElementById("todos")
+    let cachorro= document.getElementById("cachorro")
+    let gato= document.getElementById("gato")
+    let casa= document.getElementById("casa")
+    let praia= document.getElementById("praia")
+    let fim= document.getElementById("fim")
+
+    gato.classList.add("sumir")
+    casa.classList.add("sumir")
+    praia.classList.add("sumir")
+    fim.classList.add("sumir")
+    cachorro.classList.add("sumir")
+
+    inicio.classList.remove("sumir")
+}
+
+
+function Aparecer_Cachorro()
+{
+    let inicio= document.getElementById("todos")
+    let cachorro= document.getElementById("cachorro")
+    let gato= document.getElementById("gato")
+    let casa= document.getElementById("casa")
+    let praia= document.getElementById("praia")
+    let fim= document.getElementById("fim")
+
+    gato.classList.add("sumir")
+    casa.classList.add("sumir")
+    praia.classList.add("sumir")
+    fim.classList.add("sumir")
+    inicio.classList.add("sumir")
+    
+
+    cachorro.classList.remove("sumir")
+}
+
+function Aparecer_Gato()
+{
+    
+    let inicio= document.getElementById("todos")
+     let cachorro= document.getElementById("cachorro")
+     let gato= document.getElementById("gato")
+     let casa= document.getElementById("casa")
+     let praia= document.getElementById("praia")
+     let fim= document.getElementById("fim")
+    
+     cachorro.classList.add("sumir")
+     casa.classList.add("sumir")
+     praia.classList.add("sumir")
+     fim.classList.add("sumir")
+     inicio.classList.add("sumir")
+    
+     gato.classList.remove("sumir")
+}
+
+function Aparecer_Casa()
+{
+    let inicio= document.getElementById("todos")
+    let cachorro= document.getElementById("cachorro")
+    let gato= document.getElementById("gato")
+    let casa= document.getElementById("casa")
+    let praia= document.getElementById("praia")
+    let fim= document.getElementById("fim")
+   
+    cachorro.classList.add("sumir")
+    gato.classList.add("sumir")
+    praia.classList.add("sumir")
+    fim.classList.add("sumir")
+    inicio.classList.add("sumir")
+   
+    casa.classList.remove("sumir")
+}
+
+function Aparecer_Praia()
+{
+    let inicio= document.getElementById("todos")
+    let cachorro= document.getElementById("cachorro")
+    let gato= document.getElementById("gato")
+    let casa= document.getElementById("casa")
+    let praia= document.getElementById("praia")
+    let fim= document.getElementById("fim")
+   
+    cachorro.classList.add("sumir")
+    casa.classList.add("sumir")
+    gato.classList.add("sumir")
+    fim.classList.add("sumir")
+    inicio.classList.add("sumir")
+   
+    praia.classList.remove("sumir")
+}
+
+function Aparecer_Fim()
+{
+    let inicio= document.getElementById("todos")
+    let cachorro= document.getElementById("cachorro")
+    let gato= document.getElementById("gato")
+    let casa= document.getElementById("casa")
+    let praia= document.getElementById("praia")
+    let fim= document.getElementById("fim")
+   
+    cachorro.classList.add("sumir")
+    casa.classList.add("sumir")
+    praia.classList.add("sumir")
+    gato.classList.add("sumir")
+    inicio.classList.add("sumir")
+   
+    fim.classList.remove("sumir")
+}
